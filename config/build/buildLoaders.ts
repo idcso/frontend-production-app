ebe047044be30c90614aeea1e1910a211daaccf8
idcso <52316112+idcso@users.noreply.github.com>
2024-05-10 14:35:01 +0500

    Add babel with base config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -25,6 +25,17 @@ export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
     ],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -40,5 +51,5 @@ export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
     ],
   };
 
-  return [fileLoader, typescriptLoader, cssLoader, svgLoader];
+  return [fileLoader, babelLoader, typescriptLoader, cssLoader, svgLoader];
 };
