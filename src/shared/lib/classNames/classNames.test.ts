909537a4c477b5e76c7353aab9bbd486d9d44eda
idcso <52316112+idcso@users.noreply.github.com>
2024-05-10 15:24:24 +0500

    Implement and configure Jest with the first tests

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('className')).toBe('className');
+  });
+
+  test('with additional classes', () => {
+    const expected = 'className class1 class2';
+    expect(classNames('className', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with true mods', () => {
+    const expected = 'className selected hidden class1 class2';
+    expect(classNames(
+      'className',
+      {selected: true, hidden: true},
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+
+  test('with false mods', () => {
+    const expected = 'className selected class1 class2';
+    expect(classNames(
+      'className',
+      {selected: true, hidden: false},
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+
+  test('with undefined mods', () => {
+    const expected = 'className selected class1 class2';
+    expect(classNames(
+      'className',
+      {selected: true, hidden: undefined},
+      ['class1', 'class2']
+    )).toBe(expected);
+  });
+});
