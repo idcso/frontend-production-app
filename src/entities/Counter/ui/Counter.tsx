bda83d53d691f8020057dc3e7113096f26a0ccbc
idcso <52316112+idcso@users.noreply.github.com>
2024-05-19 02:47:03 +0500

    Add redux-toolkit store and create first entity
    Also changed versions of some packages to remove conflicts.

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable i18next/no-literal-string */
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'shared/ui/Button/Button';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+  const counterValue = useSelector(getCounterValue);
+  const dispatch = useDispatch();
+
+  const increment = () => dispatch(counterActions.increment());
+
+  const decrement = () => dispatch(counterActions.decrement());
+
+  return (
+    <div>
+      <h1 data-testid="value-title">{counterValue}</h1>
+      <Button onClick={increment} data-testid="increment-btn">
+        increment
+      </Button>
+      <Button onClick={decrement} data-testid="decrement-btn">
+        decrement
+      </Button>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import 'app/styles/index.scss';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import './app/styles/index.scss';
 import './shared/config/i18n/i18n';
-import 'app/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>
 );
