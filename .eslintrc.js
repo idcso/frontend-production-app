5da773edda630534096a296d0f50446466db2638
idcso <52316112+idcso@users.noreply.github.com>
2024-06-26 02:03:00 +0500

    Implement green app theme

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'react', 'i18next', 'react-hooks'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { 'SwitchCase': 1 }],
     //'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
