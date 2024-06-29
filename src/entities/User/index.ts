79b69de4c88839416a9554e26696a20385e2a5d4
idcso <52316112+idcso@users.noreply.github.com>
2024-06-30 01:50:00 +0500

    Update app router with protected routes

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,5 +1,13 @@
-import { userReducer, userActions } from './model/slice/userSlice';
-import { User, UserSchema } from './model/types/user';
 import { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+import { getUserInited } from './model/selectors/getUserInited/getUserInited';
+import { userActions, userReducer } from './model/slice/userSlice';
+import { User, UserSchema } from './model/types/user';
 
-export { User, UserSchema, userReducer, userActions, getUserAuthData };
+export {
+  User,
+  UserSchema,
+  getUserAuthData,
+  getUserInited,
+  userActions,
+  userReducer,
+};
